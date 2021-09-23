696554cd39f0119ca6de880281e002cc1e647454
Serhii T <tocarchuk@gmail.com>
2021-09-23 20:47:34 +0300

    Create simple UI: add buttons for user choice

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+const resetButton = document.querySelector('#reset');
+
+const announcer = document.querySelector('.announcer');
+
+const YOU_WON = "YOU_WON";
+const COMPUTER_WON = "COMPUTER_WON";
+const TIE = "TIE";
+
+rockButton.addEventListener('click', () => playRound('rock', computerSelection));
+paperButton.addEventListener('click', () => playRound('paper', computerSelection));
+scissorsButton.addEventListener('click', () => playRound('scissors', computerSelection));
+
 //function will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
 function computerPlay(){
   let random = Math.floor(Math.random() * 3);
   if( random === 0 ){
-    return 'Rock';
+    return 'rock';
   } else if( random === 1 ){
-    return 'Paper';
+    return 'paper';
   } else {
-    return 'Scissors';
+    return 'scissors';
   }
 };
 
@@ -20,47 +35,58 @@ function computerPlay(){
 
 // play round function
 function playRound(playerSelection, computerSelection){
+  let computerSelection = computerPlay();
   if( playerSelection === 'rock' && computerSelection === 'scissors' ){
-    console.log(`You Won Round! ${playerSelection} beats ${computerSelection}`);
+    announce(YOU_WON);
     return 1;
   } else if ( playerSelection === 'paper' && computerSelection === 'rock' ){
-    console.log(`You Won Round! ${playerSelection} beats ${computerSelection}`);
+    announce(YOU_WON);
     return 1;
   } else if ( playerSelection === 'scissors' && computerSelection === 'paper' ){
-    console.log(`You Won Round! ${playerSelection} beats ${computerSelection}`);
+    announce(YOU_WON);
     return 1;
   } else if ( playerSelection === computerSelection ){
-    console.log(`TIE! ${playerSelection} equals ${computerSelection}`);
+    announce(TIE);
     return 0;
   } else {
-    console.log(`Computer Won Round! ${computerSelection} beats ${playerSelection}`);
+    announce(COMPUTER_WON);
     return -1;
   }
 };
 
-// game function
-function game(){
-  let playerScore = 0;
-  let compScore = 0;
-  let i = 0;
- 
-  while( i < 3 ){
-    let playerSelection = prompt("Make your choice: Rock, Paper or Scissors").toLowerCase();
-    let computerSelection = computerPlay().toLowerCase();
-    let result = playRound(playerSelection, computerSelection);
-
-    if( result === 1 ){
-      playerScore++;
-    } else if( result === -1 ){
-      compScore++;
-    }
-    i++;
+const announce = (type) => {
+  switch(type){
+    case YOU_WON:
+      announcer.innerHTML = '<span class="player">You</span> Won';
+      break;
+    case COMPUTER_WON:
+      announcer.innerHTML = '<span class="computer">Computer</span> Won';
+      break;
+    case TIE:
+      announcer.innerText = 'Tie';        
   }
-  if( playerScore > compScore ){
-  console.log(`You Won Game: Final score is You: ${playerScore} vs Computer: ${compScore}`);
-  } else {
-    console.log(`Computer Won Game: Final score is You: ${playerScore} vs Computer: ${compScore}`);
-  }
+  announcer.classList.remove('hide');
 };
 
-console.log(game());
+// game function
+// function game(){
+//   let playerScore = 0;
+//   let compScore = 0;
+//   let i = 0;
+  
+//   let computerSelection = computerPlay().toLowerCase();
+//   let result = playRound(playerSelection, computerSelection);
+
+//     if( result === 1 ){
+//       playerScore++;
+//     } else if( result === -1 ){
+//       compScore++;
+//     }
+//     i++;
+//   }
+//   if( playerScore > compScore ){
+//   console.log(`You Won Game: Final score is You: ${playerScore} vs Computer: ${compScore}`);
+//   } else {
+//     console.log(`Computer Won Game: Final score is You: ${playerScore} vs Computer: ${compScore}`);
+//   }
+// };
