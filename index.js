86a3efaa7cbcf21ba23e2be71cd9b6a3ab5a2cd8
Serhii T <tocarchuk@gmail.com>
2021-09-24 12:16:47 +0300

    Add button Restart and its functionality

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const scissorsButton = document.querySelector('#scissors');
 const announcer = document.querySelector('.announcer');
 const score = document.querySelector('.score');
 const finalResult = document.querySelector('.finalResult');
+const restartButton = document.querySelector('#restart');
 
 const YOU_WON = "YOU_WON";
 const COMPUTER_WON = "COMPUTER_WON";
@@ -15,6 +16,7 @@ let isGameActive = true;
 rockButton.addEventListener('click', () => playRound('rock', computerPlay()));
 paperButton.addEventListener('click', () => playRound('paper', computerPlay()));
 scissorsButton.addEventListener('click', () => playRound('scissors', computerPlay()));
+restartButton.addEventListener('click', () => restartGame());
 
 //function will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
 function computerPlay(){
@@ -78,7 +80,6 @@ let compScore = 0;
 // function return running score
 function runningScore(playerScore, compScore){
   score.innerHTML = `Score is <span class="player">You</span>: ${playerScore} vs <span class="computer">Computer</span>: ${compScore}`;
-  score.classList.remove('hide');
 
   let result = playerScore + compScore;
   if( result === 5 ){
@@ -91,8 +92,19 @@ function finalScore(playerScore, compScore){
   if( playerScore > compScore ){
   finalResult.innerHTML = `<span class="player">YOU</span> WON GAME!`;
   finalResult.classList.remove('hide');
+  restartButton.classList.remove('hide');
   } else {
     finalResult.innerHTML = `<span class="computer">COMPUTER</span> WON GAME!`;
     finalResult.classList.remove('hide');
+    restartButton.classList.remove('hide');
   }
+};
+
+// function will allow to restart game 
+function restartGame(){
+  playerScore = 0;
+  compScore = 0;
+  announcer.classList.add('hide');
+  finalResult.classList.add('hide');
+  return runningScore(playerScore, compScore);
 };
